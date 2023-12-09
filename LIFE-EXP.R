46fc766d3505e7fda513b0d40a9f56e7467b61f6
NV-SYNERGY <nickydiaz131@gmail.com>
2023-12-09 17:13:52 -0600

    "finally got the code to work thank the lord' bar graph is a go scatteplot and checking for all sorts of metrics

--- LIFE-EXP.R
+++ LIFE-EXP.R
@@ -1,29 +1,48 @@
+#R.home("bin")
 
-
-R.home("bin")
-
-
+# DECEMBER 9TH, 2023
 #libraries needed
 library(RCurl)
 library(tidyverse)
-# Getting the .CSV file
-raw_URL <- "https://github.com/ahiller13/MATH4780-LIFE-EXPECTANCY/blob/main/Life-Expectancy-Data-Updated.csv"
-text_URL <- getURL(raw_URL)
-life_expectancy <- read.csv(text = text_URL)
 
+# Getting the url With my token
+url_w_token <- "https://raw.githubusercontent.com/ahiller13/MATH4780-LIFE-EXPECTANCY/main/LEDU.csv?token=GHSAT0AAAAAACLGJC4OLAMZY2OQP73GPKEAZLU5MVQ"
 
+# nicky token
+headers <- c('Authorization' = paste('Bearer','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'))
+
+dataset <- getURL(url_w_token,httpheader = headers)
+#print(dataset)
+
+Life_expectancy <- read.csv(text = dataset)
+
+# Checking format
+head(Life_expectancy)
+
+ggplot(Life_expectancy,aes(x= as.factor(Economy_status_Developed),y= Life_expectancy)) +
+    stat_summary(fun = mean,geom = 'bar',fill = 'skyblue',position = 'dodge') +
+    labs(title = 'Bar Graph of mean Life Expectancy by Economy status (developed)',
+        x='Economy Status (developed)',
+        y='Mean Life Expectancy') +
+        theme(plot.title = element_text(size=48,face='bold'),
+        axis.title.x = element_text(size=32),
+        axis.title.y = element_text(size=32)
+        )
+    
+
+#################### ANDREW BIT ####################
 #just how I have the data set mapped in my files on my laptop
-data <- read.csv("School/FALL23/MATH 4780/R sets/Life-Expectancy-Data-Updated.csv")
+#data <- read.csv("School/FALL23/MATH 4780/R sets/Life-Expectancy-Data-Updated.csv")
 
 # Check the structure of the dataset
-str(data)
+#str(data)
 
 #(12/06)
 #Built a basic bar graph comparing mean life expectancy for Developed and Developing countries
 
 # Create a bar graph with mean Life_expectancy
-ggplot(data, aes(x = as.factor(Economy_status_Developed), y = Life_expectancy)) +
-  stat_summary(fun = mean, geom = "bar", fill = "skyblue", position = "dodge") +
-  labs(title = "Bar Graph of Mean Life Expectancy by Economy Status (Developed)",
-       x = "Economy Status (Developed)",
-       y = "Mean Life Expectancy")
+#ggplot(data, aes(x = as.factor(Economy_status_Developed), y = Life_expectancy)) +
+#  stat_summary(fun = mean, geom = "bar", fill = "skyblue", position = "dodge") +
+#  labs(title = "Bar Graph of Mean Life Expectancy by Economy Status (Developed)",
+#       x = "Economy Status (Developed)",
+#       y = "Mean Life Expectancy")
