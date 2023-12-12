7f43c2b8403a09cbd9a05c5b8d29144bf0c3656a
ethanbaierl <126723095+ethanbaierl@users.noreply.github.com>
2023-12-12 17:00:59 -0600

    Update LIFE-EXP.R

--- LIFE-EXP.R
+++ LIFE-EXP.R
@@ -125,6 +125,7 @@ summary(car::powerTransform(full_model,family = 'bcPower'))
 dataLE <- read.csv("/Users/ethanbaierl/Downloads/Life-Expectancy-Data-Updated.csv")
 View(dataLE)
 
+# Install necessary packages
 install.packages("car")
 library(car)
 
@@ -143,26 +144,146 @@ summary(linM2) ## R2 = 0.9791
 
 
 # Remove less significant variables from model 2
-linM3 <- lm(Life_expectancy ~ Infant_deaths + Under_five_deaths + Adult_mortality + Alcohol_consumption + BMI + Incidents_HIV + GDP_per_capita + Thinness_five_nine_years + Schooling + Economy_status_Developed,
+linM3 <- lm(Life_expectancy ~ Infant_deaths + 
+              Under_five_deaths + 
+              Adult_mortality + 
+              Alcohol_consumption + 
+              BMI + Incidents_HIV + 
+              GDP_per_capita + 
+              Thinness_five_nine_years + 
+              Schooling + 
+              Economy_status_Developed,
             data = dataLE)
+
 summary(linM3) ## R2 = 0.9791
                ## Adj R2 = 0.979
-vif(linM3) ## Collinearity
+vif(linM3)
 
 linM4 <- lm(Life_expectancy ~ Infant_deaths + Adult_mortality + Alcohol_consumption + BMI + Incidents_HIV + GDP_per_capita + Thinness_five_nine_years + Schooling + Economy_status_Developed,
-            data = dataLE)  ## Remove 'Under_five_deaths'
-summary(linM4) ## R2 = 0.9774
-               ## Adj R2 = 0.9773
+            data = dataLE)
+summary(linM4)
 vif(linM4)
 
-
-##BEST MODEL?##
-linM5 <- lm(Life_expectancy ~ + Under_five_deaths + Adult_mortality + Alcohol_consumption + BMI + Incidents_HIV + GDP_per_capita + Thinness_five_nine_years + Schooling + Economy_status_Developed,
-            data = dataLE) ## Remove 'Infant_deaths'
-summary(linM5) ## R2 = 0.9787 
+install.packages("car")
+library(car)
+linM5 <- lm(Life_expectancy ~ Under_five_deaths + Adult_mortality + Alcohol_consumption + BMI + Incidents_HIV + GDP_per_capita + Thinness_five_nine_years + Schooling + Economy_status_Developed,
+            data = dataLE)
+summary(linM5) ## R2 = 0.9787
                ## Adj R2 = 0.9786
+
 vif(linM5)
 plot(linM5)
+
+#########################
+## Leverage Points
+hatLinM5 <- hatvalues(linM5)
+sortHatLinM5 <- sort(hatLinM5, decreasing = TRUE)
+
+# Calculate the threshold based on the number of predictors (p) and the sample size (n)
+p <- 9  # Number of predictors
+n <- dim(dataTest)[1]  # Sample size
+
+# Calculate the threshold (2 * p / n)
+threshold <- 2 * p / n
+
+# Find the observations with hat values greater than the threshold
+outliers <- sortHatLinM5[sortHatLinM5 > threshold]
+
+# Print or inspect the outliers
+print(outliers)
+plot(outliers)
+########################################
+### Display outliers and leverage points & Influential points
+library(olsrr)
+ols_plot_resid_lev(linM5)
+
+### Cooks D
+ols_plot_cooksd_chart(linM5)
+cooks.distance(linM5)
+
+# Influential mesaures
+influence.measures(linM5)
+
+
+###############################
+# Calculate studentized residuals
+r_student <- rstudent(linM5)
+# Round and sort the studentized residuals in decreasing order
+sorted_r_student <- round(sort(r_student, decreasing = TRUE), 2)
+# Print or inspect the sorted studentized residuals
+print(sorted_r_student)
+plot(sorted_r_student)
+
+# Calculate DFFITS
+round(dffit <- dffits(linM5), 2)
+dffit[abs(dffit) > 2 * sqrt(p/n)] 
+
+# Calculate COV Ratio
+(covra <- covratio(linM5))
+covra[covra > (1 + 3*p/n)] 
+covra[covra < (1 - 3*p/n)] 
+
+# Bubble Plot
+car::influencePlot(linM5)
+
+
+#####################
+#### Collinearity ###
+plot(dataLE$Infant_deaths, dataLE$Under_five_deaths, 
+  main = "Infant Deaths vs Under 5 y/o Deaths",
+  xlab = "Infant Deaths",
+  ylab = "Under 5 y/o Deaths",
+)
+
+plot(dataLE$Polio, dataLE$Diphtheria, 
+     main = "Polio vs Diphtheria",
+     xlab = "Polio",
+     ylab = "Diptheria",
+)
+
+#################
+## train & test model set
+install.packages("caret")
+library(caret)
+install.packages("magrittr")
+library(magrittr)
+
+# Set seed for reproducibility/randomness
+set.seed(1)
+
+## Create new dataset using the variables from linM5 (best model)
+dataTest <- dataLE[c("Under_five_deaths",
+                     "Adult_mortality",
+                     "Alcohol_consumption",
+                     "BMI",
+                     "Incidents_HIV",
+                     "GDP_per_capita",
+                     "Thinness_five_nine_years",
+                     "Schooling",
+                     "Economy_status_Developed",
+                     "Life_expectancy"
+                     )]
+                     
+                     
+
+# Split the data into training (70%) and validation (30%) sets
+splitIndex <- createDataPartition(dataTest$Life_expectancy,
+                                  p = 0.7, list = FALSE)
+
+trainData <- dataTest[splitIndex, ]
+validData <- dataTest[-splitIndex, ]
+
+modellm <- lm(Life_expectancy ~., trainData)
+summary(modellm)
+prediction <- modellm %>% predict(validData)
+modelError <- RMSE(predict, validData$Life_expectancy) ## 1.33
+modelAccuracy <- R2(predict, validData$Life_expectancy) ## 0.9795
+
+# Formula to find error rate using RMSE
+errorRate <- modelError/mean(validData$Life_expectancy) ## 1.93%
+errorRate
+
+
 
 
 ##################################
